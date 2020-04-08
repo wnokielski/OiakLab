9e1863d92c31ac7d6f01acd8c439201650adb76d
Wojciech Nokielski <wojciech.nokielski@gmail.com>
2020-04-08 17:38:26 +0200

    Dzialajace dodawanie

--- lab2/mul.s
+++ lab2/mul.s
@@ -1,6 +1,13 @@
 SYSEXIT = 1
 EXIT_SUCCESS = 0
 
+.data
+liczba1:
+        .long 0x10304008, 0x701100FF, 0x45100020, 0x08570030
+liczba2:
+        .long 0xF040500C, 0x00220026, 0x321000CB, 0x04520031
+
+
 .global _start
 _start:
 
